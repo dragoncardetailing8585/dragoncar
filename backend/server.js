cbac990dcdc02a3877c0bdf75a3a5c095d56f094
Rohan <rajputrohan196@gmail.com>
2025-08-25 17:21:38 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ config();
 import bookingRoutes from "./routes/bookingRoutes.js";
 
 const app = express();
-app.use(cors());
+app.use(cors({  
+  origin: ["https://dragoncardetailing.com"],
+  methods: ["GET", "POST"],
+}));
 app.use(json());
 
 app.use("/api/bookings", bookingRoutes);
